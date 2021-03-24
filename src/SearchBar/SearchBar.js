d9211d345e46c038fdcb488508184f95376f22ef
Christopher 'ZK' Hal <zkzerotwo@gmail.com>
2021-03-24 17:40:13 -0500

    search returns results

--- src/SearchBar/SearchBar.js
+++ src/SearchBar/SearchBar.js
@@ -1,21 +1,116 @@
 import React from 'react'
+// import { textChangeRangeIsUnchanged } from 'typescript';
+import config from '../config';
 
 export default class SearchBar extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            query: {
+                q: ""
+            },
+            // search: "",
+            // searchType: "",
+            options: [
+                "anime", "manga", "person", "character"
+            ],
+        }
+    }
+    static defaultProps = {
+
+    }
+    formatQueryParams(params) {
+        console.log(params, "queryczech")
+        const queryItems = Object.keys(params)
+            .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
+        return queryItems.join('&');
+    }
+    handleSubmit(e) {
+        e.preventDefault();
+        // console.log(this.state)
+        this.formatQueryParams(this.state.query)
+        const url = `${config.API_ENDPOINT}`
+        const pass = `${config.API_TOKEN}`
+        const query = this.state.searchType + "?" + this.formatQueryParams(this.state.query)
+        // this.state.search
+        const options = {
+            "method": "GET",
+            "headers": {
+                "x-rapidapi-key": pass,
+                "x-rapidapi-host": "jikan1.p.rapidapi.com"
+            }
+        }
+        const fullUrl = url + query
+        console.log(fullUrl, pass, "url check")
+        fetch(fullUrl,
+            options)
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Something went wrong, please try again later.');
+                }
+                return response
+            })
+            .then(response => response.json())
+            .then(response => console.log(response.results))
+            .catch(err => {
+                console.error(err);
+            });
+    }
+    // updateSearchBar(value) {
+    // console.log(value, "searchbar value")
+    //     this.setState({
+    //         search: value
+    //     })
+    // }
+    updateSearchType(type) {
+        // console.log(type, "selected")
+        this.setState({
+            searchType: type
+        })
+    }
+    updateSearchQuery(find) {
+        // console.log(find, "queryCheck1")
+        this.setState({
+            query: {
+                q: find
+            }
+
+        })
+    }
     render() {
+        const searchType = this.state.options
+            .map(select => {
+                return (
+
+                    <option key={select.toString()} value={select}>{select}</option>
+                )
+            })
+        // console.log(searchType, "checking search type")
+        // console.log(this.state, "state check")
         return (
             <section className='search-bar'>
-                <form>
+                <form onSubmit={e => this.handleSubmit(e)}>
                     <label>
                         <input type='text'
-                        id='keyword'
-                        name='keyword'
-                        placeholder='Search by keyword or genre'
-                        // value='action'
-                        required
+                            id='search'
+                            name='search'
+                            placeholder='Search by keyword or genre'
+                            onChange={e => this.updateSearchQuery(e.target.value)}
+                            required
                         ></input>
                     </label>
+                    <label
+                        htmlFor="search-type"
+                    >
+                        <select
+                            id="search-type"
+                            name="search-type"
+                            defaultValue="anime"
+                            onChange={e => this.updateSearchType(e.target.value)}>
+                            {searchType}
+                        </select>
+                    </label>
                     <p>Hey there!</p>
-
                     <button>
                         Submit!
                     </button>
---
+++ src/config.js
@@ -0,0 +1,5 @@
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+    API_ENDPOINT: "https://jikan1.p.rapidapi.com/search/",
+    API_TOKEN: "d36a838384mshc96686c738af0aap11fd78jsnf972d810e8ef"
+}
